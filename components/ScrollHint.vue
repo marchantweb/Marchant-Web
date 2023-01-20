27147c80ca16418fd4c6b581b67c01d07c250037
Simon Le Marchant <simon@marchantweb.com>
2023-01-20 11:50:55 -0500

    Added scroll hinting.

---
+++ components/ScrollHint.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="scroll-hint mouse-lg">
+    <span>Scroll to Explore</span> <i class="fa-light fa-computer-mouse-scrollwheel fa-2x"></i>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+.scroll-hint {
+
+  border-bottom: 2px solid white;
+  padding-bottom: 30px;
+  min-width: 300px;
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+
+  span {
+    color: white;
+    font-weight: 600;
+    margin-right: 30px;
+    pointer-events: none;
+  }
+
+  i {
+    color: white;
+    pointer-events: none;
+  }
+}
+
+</style>
