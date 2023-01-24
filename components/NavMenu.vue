0dd20e72a6e09092635c167391bb6f04ae78f8a0
Simon <simon@marchantweb.com>
2023-01-24 17:04:56 -0500

    Starting to implement animated nav.

--- components/NavMenu.vue
+++ components/NavMenu.vue
@@ -1,19 +1,28 @@
 <template>
-  <section class="container pt-6">
-    <div class="row">
-      <div class="col text-end">
-        <NuxtLink to="/about">
-          <button class="me-3">About</button>
-        </NuxtLink>
-        <NuxtLink to="/portfolio">
-          <button class="me-3">Portfolio</button>
-        </NuxtLink>
-        <NuxtLink to="/contact">
-          <button class="me-3">Contact</button>
-        </NuxtLink>
+  <div class="container mt-6">
+    <div class="row justify-content-end">
+      <div class="col-auto">
+        <section class="nav-container nav-container--open">
+          <a href="#" class="nav-icon mouse-md">
+            <div class="nav-icon__line"></div>
+            <div class="nav-icon__line"></div>
+            <div class="nav-icon__line"></div>
+          </a>
+          <nav class="nav-links">
+            <NuxtLink to="/about">
+              <button>About</button>
+            </NuxtLink>
+            <NuxtLink to="/portfolio">
+              <button>Portfolio</button>
+            </NuxtLink>
+            <NuxtLink to="/contact">
+              <button>Contact</button>
+            </NuxtLink>
+          </nav>
+        </section>
       </div>
     </div>
-  </section>
+  </div>
 </template>
 
 <script setup>
@@ -21,5 +30,56 @@
 </script>
 
 <style lang="scss" scoped>
+
+.nav-container {
+  position: relative;
+
+  .nav-icon {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 20px;
+    width: 30px;
+    cursor: pointer;
+  }
+
+  .nav-icon__line {
+    height: 2px;
+    width: 100%;
+    background-color: #FFFFFF;
+    box-shadow: rgba(0, 0, 0, 0.19) 0 10px 20px, rgba(0, 0, 0, 0.23) 0 6px 6px;
+    pointer-events: none;
+  }
+
+  .nav-links {
+    clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+    transition: clip-path 0.5s ease-in-out;
+    position: absolute;
+    top: -10px;
+    right: 90px;
+    display: flex;
+    flex-direction: row;
+    gap: 30px;
+
+    button{
+      background: none;
+      border: none;
+      padding: 10px 30px;
+      margin: 0;
+      color: white;
+      font-family: 'Inter', sans-serif;
+      font-weight: 600;
+      letter-spacing: 0.2px;
+      text-shadow: 0 22px 30px rgba(0, 0, 0, 0.4), 2px 2px 1px rgba(206, 89, 55, 0);
+    }
+  }
+
+  &.nav-container--open{
+    .nav-links{
+      clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
+    }
+  }
+
+}
 
 </style>
--- pages/portfolio/index.vue
+++ pages/portfolio/index.vue
@@ -1,13 +1,7 @@
 <template>
   <section class="page">
 
-    <div class="container">
-      <div class="row mt-5">
-        <div class="col">
-          <MWIcon :size="50"/>
-        </div>
-      </div>
-    </div>
+    <NavMenu />
 
     <ProjectGallery />
 
