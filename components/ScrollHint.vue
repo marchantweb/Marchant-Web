167702f856fb2d75a37491f4690c14f9d0433351
Simon Le Marchant <simon@marchantweb.com>
2023-02-14 09:44:44 -0500

    Animated ScrollHint

--- components/ScrollHint.vue
+++ components/ScrollHint.vue
@@ -13,9 +13,23 @@
 .scroll-hint {
   border-bottom: 1px solid rgba(255,255,255,0.2);
   padding-bottom: 20px;
-  min-width: 300px;
+  width: 300px;
   justify-content: flex-end;
   align-items: flex-end;
+  position: relative;
+
+  &:before{
+    content: "";
+    position: absolute;
+    bottom: -1px;
+    left: 0;
+    width: 50px;
+    height: 1px;
+    background-color: white;
+    display: block;
+    animation: loopingBorder 2s linear infinite;
+    transform-origin: 0 center;
+  }
 
   span {
     color: #F2F2F2;
@@ -27,6 +41,27 @@
   i {
     color: #F2F2F2;
     pointer-events: none;
+  }
+}
+
+@keyframes loopingBorder{
+  0%{
+    transform: translateX(0) scaleX(0);
+    transform-origin: 0 center;
+  }
+  40%{
+    transform: translateX(150px) scaleX(1);
+    transform-origin: 50% center;
+  }
+  80%{
+    width: 0;
+    transform: translateX(300px) scaleX(0);
+    transform-origin: 100% center;
+  }
+  100%{
+    width: 0;
+    transform: translateX(300px) scaleX(0);
+    transform-origin: 100% center;
   }
 }
 
