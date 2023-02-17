87499565b3dec5aa11108d38ff7d69cbc28c19a9
Simon Le Marchant <simon@marchantweb.com>
2023-02-17 15:40:44 -0500

    Added preconnect to api.marchantweb.com

--- app.vue
+++ app.vue
@@ -35,7 +35,10 @@ useHead({
       content: 'Simon is a senior, creative front-end developer who works with the latest technologies, from Nuxt and Vue to WebGL and HTML5 Canvas. With over 10 years of experience in the creative front-end development field, he has a wealth of knowledge and expertise in the latest front-end technologies. He is passionate about creating innovative, user-friendly web experiences that stand out from the crowd. With his creativity and attention to detail, he will bring your project to life.'
     },
   ],
-  link: [{rel: 'icon', type: 'image/svg', href: '/icon-notext.svg'}],
+  link: [
+    {rel: 'icon', type: 'image/svg', href: '/icon-notext.svg'},
+    {rel: 'preconnect', href: 'https://api.marchantweb.com'},
+  ],
   script: [
     {
       src: "https://kit.fontawesome.com/172ddc2486.js", async: true, defer: true
