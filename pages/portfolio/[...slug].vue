9d60d4ae5075e86373a1a9030dafb37fe203b4bc
Simon Le Marchant <simon@marchantweb.com>
2023-02-22 14:05:48 -0500

    Added copyright and minor tweaks to case studies.

--- pages/portfolio/[...slug].vue
+++ pages/portfolio/[...slug].vue
@@ -26,9 +26,9 @@
         </div>
       </div>
 
-      <div class="row mt-5 mt-lg-8 mt-xl-7 gx-6 gx-xl-8 pb-5 pb-lg-7">
+      <div class="row mt-5 mt-lg-8 mt-xl-7 gx-6 gx-xl-8 pb-3">
         <div class="col-12 col-lg-4 order-2 order-lg-1 pt-6 pt-lg-0">
-          <section class="ps-0 ps-lg-6" style="position: sticky; top: 60px">
+          <section class="ps-0 ps-lg-6 mb-5 mb-lg-0" style="position: sticky; top: 60px">
             <h3 class="mb-5" data-aos="fade-up">Details</h3>
             <dl class="mb-5 d-block" data-aos="fade-up">
 
@@ -46,8 +46,8 @@
 
             </dl>
             <ActionButton to="https://calendly.com/marchantweb/discovery" target="_blank" data-aos="fade-up">
-              <i class="fa-sharp fa-solid fa-phone-arrow-up-right"></i>
-              Schedule a call
+              <i class="fa-sharp fa-regular fa-calendar-range fa-lg"></i>
+              Book some time
             </ActionButton>
           </section>
         </div>
@@ -61,6 +61,7 @@
             <h1 class="mb-4 mb-lg-5" data-aos="fade-up">{{ currentPortfolioItem['lead'] }}</h1>
           </div>
           <NotionContent :blocks="currentPortfolioItem['pageContent']"/>
+          <p class="mt-7 mt-xxl-8 mt-xxxl-9 text-small text-end copyright d-none d-lg-block">Copyright © {{new Date().getFullYear()}} Marchant Web, LLC. All rights reserved.</p>
         </div>
       </div>
 
@@ -121,9 +122,19 @@ video {
   }
 }
 
-p {
+p:not(.copyright) {
   width: 700px;
   max-width: 100%;
+}
+
+.copyright{
+  opacity: 0.5;
+  font-size: 12px;
+  border-top: 1px solid #CCC;
+  max-width: 500px;
+  padding-top: 20px;
+  padding-left: 40px;
+  float: right;
 }
 
 </style>
