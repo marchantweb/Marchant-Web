ff68860b086d7bd668784e77939ce02b4fb1e02d
Simon Le Marchant <simon@marchantweb.com>
2023-02-23 09:11:05 -0500

    Added link to ScrollHint

--- components/ScrollHint.vue
+++ components/ScrollHint.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="scroll-hint mouse-md d-none d-xl-flex">
+  <NuxtLink to="/portfolio" class="scroll-hint mouse-md d-none d-xl-flex">
     <span>Scroll to Explore</span> <i class="fa-light fa-computer-mouse-scrollwheel fa-2x"></i>
-  </div>
+  </NuxtLink>
 </template>
 
 <script setup>
@@ -11,14 +11,14 @@
 <style lang="scss" scoped>
 
 .scroll-hint {
-  border-bottom: 1px solid rgba(255,255,255,0.2);
+  border-bottom: 1px solid rgba(255, 255, 255, 0.2);
   padding-bottom: 20px;
   width: 300px;
   justify-content: flex-end;
   align-items: flex-end;
   position: relative;
 
-  &:before{
+  &:before {
     content: "";
     position: absolute;
     bottom: -1px;
@@ -52,32 +52,32 @@
   }
 }
 
-@keyframes animateContentIn{
-  0%{
+@keyframes animateContentIn {
+  0% {
     opacity: 0;
     transform: translateY(20px);
   }
-  100%{
+  100% {
     opacity: 1;
     transform: translateY(0);
   }
 }
 
-@keyframes loopingBorder{
-  0%{
+@keyframes loopingBorder {
+  0% {
     transform: translateX(0) scaleX(0);
     transform-origin: 0 center;
   }
-  40%{
+  40% {
     transform: translateX(150px) scaleX(1);
     transform-origin: 50% center;
   }
-  80%{
+  80% {
     width: 0;
     transform: translateX(300px) scaleX(0);
     transform-origin: 100% center;
   }
-  100%{
+  100% {
     width: 0;
     transform: translateX(300px) scaleX(0);
     transform-origin: 100% center;
