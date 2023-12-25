c69f8a5155e0a23b3c5bee5b02b9e7c869c37a6a
Simon Le Marchant <simon@marchantweb.com>
2023-12-24 21:44:16 -0500

    Improved article OG tags, with dynamic cover images from article content

--- pages/article/[...slug].vue
+++ pages/article/[...slug].vue
@@ -7,7 +7,7 @@
 
       <div class="row mt-5">
         <div class="col-auto">
-          <MWIcon :size="50" :type="'notext'" />
+          <MWIcon :size="50" :type="'notext'"/>
         </div>
       </div>
 
@@ -19,14 +19,17 @@
             <div class="d-flex flex-row align-items-center mb-4">
               <img src="~assets/images/headshot.jpg" alt="Simon Le Marchant" class="article__headshot me-4 mouse-sm">
               <div>
-                <label class="article__author" itemprop="name">Simon Le Marchant · <a href="https://twitter.com/marchantweb" target="blank" class="article__followlink mouse-sm">Follow on 𝕏</a></label>
-                <span class="article__readtime">{{ readTime}} · {{ formattedDate }} </span>
+                <label class="article__author" itemprop="name">Simon Le Marchant · <a
+                    href="https://twitter.com/marchantweb" target="blank" class="article__followlink mouse-sm">Follow on
+                  𝕏</a></label>
+                <span class="article__readtime">{{ readTime }} · {{ formattedDate }} </span>
               </div>
             </div>
             <hr class="mb-6">
           </div>
           <NotionContent class="article__content" :isArticle="true" :blocks="currentArticleItem['pageContent']"/>
-          <p class="mt-7 mt-xxl-8 mt-xxxl-9 text-small text-end copyright d-none d-lg-block">Copyright © {{new Date().getFullYear()}} Marchant Web, LLC. All rights reserved.</p>
+          <p class="mt-7 mt-xxl-8 mt-xxxl-9 text-small text-end copyright d-none d-lg-block">Copyright ©
+            {{ new Date().getFullYear() }} Marchant Web, LLC. All rights reserved.</p>
         </main>
       </div>
 
@@ -37,6 +40,7 @@
 
 <script setup>
 import sugar from 'sugar';
+
 sugar.extend();
 
 const route = useRoute();
@@ -46,6 +50,10 @@ const currentArticleItem = computed(() => {
   return articleData.value[articleData.value.findIndex(article => article["slug"] === route.params["slug"][0])];
 });
 
+/**
+ * Calculates the read time of the current article
+ * @type {ComputedRef<string>}
+ */
 const readTime = computed(() => {
   let totalTextLength = 0;
   currentArticleItem.value['pageContent'].forEach((block) => {
@@ -57,6 +65,21 @@ const readTime = computed(() => {
   });
   const readTimeMinutes = Math.ceil(totalTextLength / 100);
   return `${readTimeMinutes} min read`;
+});
+
+/**
+ * Find the cover image of the article, or use the default.
+ * @type {ComputedRef<string>}
+ */
+const coverImage = computed(() => {
+  let coverImage = 'https://marchantweb.com/cover.jpg';
+  for (let block of currentArticleItem.value['pageContent']) {
+    if (block.type === 'image') {
+      coverImage = block['image']['file']['url'];
+      break;
+    }
+  }
+  return coverImage;
 });
 
 /**
@@ -80,59 +103,72 @@ const formattedDate = computed(() => {
 
 
 useHead({
-  title: currentArticleItem.value['title'] + ' | ' + 'Marchant Web',
+  title: currentArticleItem.value['title'],
   meta: [
-    { hid: 'description', name: 'description', content:  currentArticleItem.value['title'] },
-    { hid: 'og:title', property: 'og:title', content: currentArticleItem.value['title'] },
-    { hid: 'og:url', property: 'og:url', content: 'https://marchantweb.com' + route.fullPath },
-    { hid: 'og:description', property: 'og:description', content: currentArticleItem.value['title'] },
-    { hid: 'og:image', property: 'og:image', content: currentArticleItem.value['cover']},
+    {hid: 'description', name: 'description', content: currentArticleItem.value['lead']},
+    {hid: 'og:title', property: 'og:title', content: currentArticleItem.value['title']},
+    {hid: 'og:url', property: 'og:url', content: 'https://marchantweb.com' + route.fullPath},
+    {hid: 'og:description', property: 'og:description', content: currentArticleItem.value['lead']},
+    {hid: 'og:image', property: 'og:image', content: coverImage.value},
 
     // twitter card
-    { hid: "twitter:title", name: "twitter:title", content: currentArticleItem.value['title'] },
-    { hid: "twitter:url", name: "twitter:url", content: 'https://marchantweb.com' + route.fullPath },
-    { hid: 'twitter:description', name: 'twitter:description', content: currentArticleItem.value['title'] },
-    { hid: "twitter:image", name: "twitter:image", content: currentArticleItem.value['cover']},
+    {hid: "twitter:title", name: "twitter:title", content: currentArticleItem.value['title']},
+    {hid: "twitter:url", name: "twitter:url", content: 'https://marchantweb.com' + route.fullPath},
+    {hid: 'twitter:description', name: 'twitter:description', content: currentArticleItem.value['lead']},
+    {hid: "twitter:image", name: "twitter:image", content: coverImage.value},
   ],
   link: [
-    { hid: "canonical", rel: "canonical", href: 'https://marchantweb.com' + route.fullPath },
+    {hid: "canonical", rel: "canonical", href: 'https://marchantweb.com' + route.fullPath},
   ],
   bodyAttrs: {
     class: 'enable-scroll'
   }
 });
 
+useSchemaOrg([
+  {
+    '@type': 'Article',
+    'headline': currentArticleItem.value['title'],
+    'description': currentArticleItem.value['lead'],
+    'image': coverImage.value,
+    'author': {
+      'name': 'Simon Le Marchant',
+      'url': 'https://marchantweb.com'
+    },
+  }
+])
+
 </script>
 
 <style lang="scss" scoped>
 
-.article__lead{
+.article__lead {
   color: #6e829c;
   font-weight: 500;
 }
 
-.article__readtime{
+.article__readtime {
   color: #6e829c;
   font-weight: 400;
   display: block;
   font-size: 14px;
 }
 
-.article__followlink{
+.article__followlink {
   color: #7A4FEE;
 }
 
-.article__author{
+.article__author {
   font-weight: 600;
   margin-bottom: 0;
 }
 
-.article__headshot{
+.article__headshot {
   width: 65px;
   border-radius: 50%;
 }
 
-.copyright{
+.copyright {
   opacity: 0.5;
   font-size: 12px;
   border-top: 1px solid #CCC;
