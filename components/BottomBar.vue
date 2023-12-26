7de1373407535ec7a804eb778e1e5f9db133377f
Simon Le Marchant <simon@marchantweb.com>
2023-12-25 20:33:44 -0500

    Added LinkedIn and updated X logo

--- components/BottomBar.vue
+++ components/BottomBar.vue
@@ -2,7 +2,7 @@
   <div class="row mb-5 align-items-end d-none d-lg-flex">
     <div class="col-auto d-flex align-items-center" itemscope itemtype="https://schema.org/Person">
       <MWIcon/>
-      <h1 class="bottom-navigation__title ms-4"><span itemprop="name">Simon Le Marchant</span><br><span itemprop="jobTitle">Creative Software Engineer</span></h1>
+      <h1 class="bottom-navigation__title ms-4"><span itemprop="name">Simon Le Marchant</span><br><span itemprop="jobTitle">Creative Front End Engineer</span></h1>
     </div>
     <div class="col-5 col-xl-3 ms-5 mb-3 me-auto">
       <ul class="tech-stack">
@@ -13,7 +13,8 @@
       </ul>
     </div>
     <div class="col-auto mb-3 d-flex align-items-center">
-      <NuxtLink to="https://twitter.com/marchantweb" target="_blank" class="me-4 text-decoration-none" aria-label="Find me on Twitter"><i class="fa-brands fa-twitter fa-xl twitter-icon mouse-sm d-none d-xl-inline text-decoration-none" title="Find me on Twitter"></i></NuxtLink>
+      <NuxtLink to="https://www.linkedin.com/in/marchantweb/" target="_blank" class="me-4 text-decoration-none" aria-label="Find me on LinkedIn"><i class="fa-brands fa-linkedin fa-xl twitter-icon mouse-sm d-none d-xl-inline text-decoration-none" title="Find me on LinkedIn"></i></NuxtLink>
+      <NuxtLink to="https://twitter.com/marchantweb" target="_blank" class="me-4 text-decoration-none" aria-label="Find me on Twitter"><i class="fa-brands fa-x-twitter fa-xl twitter-icon mouse-sm d-none d-xl-inline text-decoration-none" title="Find me on Twitter"></i></NuxtLink>
       <NuxtLink to="https://github.com/marchantweb/Marchant-Web" target="_blank" class="me-6 text-decoration-none" aria-label="View this site on GitHub"><i class="fa-brands fa-github fa-xl github-icon mouse-sm d-none d-xl-inline text-decoration-none" title="View this site on GitHub"></i></NuxtLink>
       <StatusIndicator class="d-none d-xl-block"/>
     </div>
